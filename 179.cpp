86f5160be7d148067ddc253324d4ee0a8c85d35c
ChrisXu1006 <rx37@cornell.edu>
2015-01-20 10:25:04 -0500

    refined version of Q179

--- 179.cpp
+++ 179.cpp
@@ -26,51 +26,36 @@ bool compare(string a, string b){
         return 0;
 }
 
-void sortstr(vector<string> &strnum, vector<int> &num){
-    if ( strnum.empty() ){
-        strnum.push_back(to_string(num.back()));
-        num.pop_back();
+string largestNumber(vector<int> &num) {
+    vector<string> strnum;
+    for ( int i = 0; i < num.size(); i++ )
+        strnum.push_back(to_string(num[i]));
+    
+    sort(strnum.begin(), strnum.end(), compare);
+    
+    string ret;
+    
+    for ( int i = 0; i < strnum.size(); i++ )
+        ret = ret + strnum[i];
+    
+    /* That's in case some cheeky person decides to give us zeroes only. */
+    if ((ret[0] == '0') && (ret[ret.length() - 1] == '0')) {
+        return "0";
     }
     
-    for (int j = num.size() - 1; j >= 0; j--){
-        int i = strnum.size() - 1;
-        for ( ;i >= 0; i-- ){
-            if ( compare(strnum[i],to_string(num[j])) ){
-                if ( i == strnum.size() - 1 )
-                    strnum.push_back(to_string(num[j]));
-                else
-                    strnum.insert(strnum.begin()+i+1, to_string(num[j]));
-                break;
-            }
-        }
-        if ( i == -1 ){
-            //if ( num.back() != 0 || strnum[0] != "0")
-                strnum.insert(strnum.begin(),to_string(num[j]));
-        }
-    }
-    return;
-}
-
-string largestNumber(vector<int> &num){
-    vector<string> strnum;
-    sortstr(strnum, num);
-    string res = "";
-    for ( int i = 0; i < strnum.size(); i++ )
-        res = res + strnum[i];
-    if ( res.length() > 1 && res[0] == '0' )
-        res = "0";
-    return res;
+    return ret;
 }
 
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
-    int a[] = {34, 45, 9, 23};
+    int a[] = {5, 23, 45, 9, 8};
 	vector<int> num;
-	for (int i = 0; i < 4; i++ )
+	for (int i = 0; i < 5; i++ )
 		num.push_back(a[i]);
     cout << largestNumber(num) << endl;
     return 0;
 }
+
 
