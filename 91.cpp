7197a998bca83b824228f48caae5493aceee2c16
ChrisXu1006 <rx37@cornell.edu>
2015-01-29 10:26:24 -0500

    Q91's Solution

--- 91.cpp
+++ 91.cpp
@@ -6,50 +6,81 @@
 #include <vector>
 #include <queue>
 #include <stack>
-//#include <unodered_map>
+#include <unodered_map>
 
 using namespace std;
 
-int numDecodings(string s){
+  int numDecodings1(unordered_map<string,int> &table, string s){
 	
-	if ( s.length() == 0 )
-		return 0;
-	
-	if ( s.length() == 1 && s[0] != '0' ){
-		return 1;
-	}
-	else if ( s.length() == 1 ){
-		return 0;
-	}
-	
-	if (s.length() == 2){
-		if ( s[0] == '0' )
-			return 0;
-		else if ( s[0] <= '2' && s[1] == '0')
-			return 1;
-		else if ( s[0] <= '2' && s[1] <= '6')
-			return 2;
-	}
+	if (table.find(s) != table.end())
+		return table[s];
 	
 	int result = 0;
 	int len = s.length();
 	
-	if (s[0] == '0')
+	if ( len == 0 )
+		return 0;
+	
+	if ( len == 1 && s[0] != '0' ){
+		pair<string, int> element(s, 1);
+		table.insert(element);
+		return 1;
+	}
+	else if ( len == 1 ){
+		pair<string, int> element(s, 0);
+		table.insert(element);
+		return 0;
+	}
+	
+	if (len == 2){
+		if ( s[0] == '0' ){
+			result = 0;
+			pair<string, int> element(s, 0);
+			table.insert(element);
+			return result;
+		}
+		else if ( s[0] <= '2' && s[1] == '0'){
+			result = 1;
+			pair<string, int> element(s, 1);
+			table.insert(element);
+			return result;
+		}
+		else if ( stoi(s) <= 26 ){
+			result = 2;
+			pair<string, int> element(s, 2);
+			table.insert(element);
+			return result;
+		}
+	}
+	
+	
+	if (s[0] == '0'){
 		result = 0;
+		pair<string, int> element(s, 0);
+		table.insert(element);
+		return result;
+	}
 	
 	else{
 		if ( s[0] >= '1' && s[0] <= '9')
-			result += 1*numDecodings(s.substr(1, len-1));
+			result += 1*numDecodings1(table,s.substr(1, len-1));
 		
 		if ( s[0] <= '2' && s[1] == '0')
-			result += 1*numDecodings(s.substr(2, len-2));
+			result += 1*numDecodings1(table,s.substr(2, len-2));
 		
-		else if ( s[0] <= '2' && s[1] <= '6')
-			result += 1*numDecodings(s.substr(2, len-2));
+		else if ( stoi(s.substr(0,2)) <= 26 )
+			result += 1*numDecodings1(table,s.substr(2, len-2));
 	}
+	pair<string, int> element(s, result);
+	table.insert(element);
 	return result;
 }
 
+int numDecodings(string s){
+	unordered_map<string, int> table;
+	int result = numDecodings1(table, s);
+	return result;
+}
 int main(){
 	string s = "17";
 	cout << numDecodings(s);
