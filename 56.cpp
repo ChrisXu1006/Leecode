66b91a7361bf2c581a04a4e6d8d8dc2d2807bfdf
ChrisXu1006 <rx37@cornell.edu>
2015-02-09 20:31:28 -0500

    Revised Q56's Solution

--- 56.cpp
+++ 56.cpp
@@ -6,25 +6,55 @@
 
 using namespace std;
 
-int firstMissingPositive(int A[], int n) {
+struct Interval {
+	int start;
+	int end;
+	Interval() : start(0), end(0) {}
+	Interval(int s, int e) : start(s), end(e) {}
+};
+
+bool myfunc(Interval a, Interval b){
+	return a.start <= b.start;
+}
+
+vector<Interval> merge(vector<Interval> &intervals){
 	
-	for ( int i = 0; i < n; i++ ){
-		while ( A[i] != i + 1 ){
-			if ( A[i] <= 0 || A[i] > n || A[i] == A[A[i] -1] )
+	vector<Interval> result;
+	
+	int size = intervals.size();
+	if ( size == 0 )
+		return result;
+	
+	sort(intervals.begin(), intervals.end(), myfunc)
+	result.push_back(intervals[0]);
+	
+	for ( int i = 1; i < size; i++ ){
+		int cur = 0;
+		while ( cur < result.size() ){
+			if ( intervals[i].start >= result[cur].start && intervals[i].start <= result[cur].end ){
+				result[cur].start = min(intervals[i].start, result[cur].start);
+				result[cur].end   = max(intervals[i].end, result[cur].end);
 				break;
-			int temp = A[i];
-			A[i] = A[temp-1];
-			A[temp-1] = temp;
+			}
+			
+			if ( intervals[i].end >= result[cur].start && intervals[i].end <= result[cur].end){
+				result[cur].start = min(intervals[i].start, result[cur].start);
+				result[cur].end   = max(intervals[i].end, result[cur].end);
+				break;
+			}
+			
+			if ( intervals[i].end >= result[cur].end && intervals[i].start <= result[cur].start){
+				result[cur].start = min(intervals[i].start, result[cur].start);
+				result[cur].end   = max(intervals[i].end, result[cur].end);
+				break;
+			}
+			
+			cur++;
 		}
+		if ( cur == size )
+			result.push_back(intervals[i]);
 	}
-	
-	for ( int i = 0; i < n; i++ ){
-		if ( A[i] != i + 1 )
-			return i+ 1;
-	}
-	return n + 1;
-	
-	
+	return result;
 }
 
 int main(){
