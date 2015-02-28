3617a223037426af54129fbc349a4709599c7d6f
ChrisXu1006 <rx37@cornell.edu>
2015-02-27 22:44:24 -0500

    Q127's Solution

--- 127.cpp
+++ 127.cpp
@@ -6,17 +6,55 @@
 #include <queue>
 #include <stack>
 #include <unordered_set>
+#include <map>
 
 using namespace std;
 
-int ladderrecur(string start, string end, unordered_set<string> &visited, unordered_set<string> &dict, int step){
-	if ( start == end )
-		return step+1;
-}
-
 int ladderLength(string start, string end, unordered_set<string> &dict){
-	unorder_set<string> visited;
-	return ladderrecur(start, end, visited, dict, 0);
+	map<string, int> distance;
+		queue<string> S;
+		S.push(start);
+		distance[start] = 1;
+		int size = S.size();
+		int found = 0;
+    
+	    if ( start == end )
+	        return 1;
+	
+		while (size){
+			for ( int i = 0; i < size; i++ ){
+				int len = S.front().length();
+				for ( int j = 0; j < len; j++ ){
+					for ( char c = 'a'; c <= 'z'; c++ ){
+						if ( c == S.front()[j] )
+							continue;
+						string ns = S.front();
+						ns[j] = c;
+	                    if ( ns == end ){
+	                        distance[end] = distance[S.front()]+1;
+	                        found = 1;
+	                        break;
+	                    }
+						if ( dict.find(ns) != dict.end() ){
+							S.push(ns);
+							dict.erase(ns);
+							distance[ns] = distance[S.front()]+1;
+						}
+						else
+							continue;
+					}
+	                if (found)
+	                    break;
+				}
+	            if(found)
+	                break;
+				S.pop();
+			}
+			size = S.size();
+			if ( found == 1 )
+				break;
+		}
+		return distance[end];
 }
 
 int main(){
