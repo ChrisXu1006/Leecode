b24d9d65f9590c90d0d2a34bfb098b2e4be019e1
ChrisXu1006 <rx37@cornell.edu>
2015-04-11 22:32:32 -0400

    Q41's Solution

--- 41.cpp
+++ 41.cpp
@@ -1,47 +1,48 @@
 #include <iostream>
+#include <stdio.h>
 #include <cstring>
 #include <string>
-#include <stack>
 #include <vector>
+#include <queue>
+#include <stack>
 
 using namespace std;
 
-int trap(int A[], int n){
-	
-	int contain = 0;
-	
-	if ( n == 0 )
-		return 0;
-	
-	int maxleft[n];
-	int maxright[n];
-	
-	int max = A[0];
-	
-	for ( int i = 1; i < n; i++ ){
-		maxleft[i] = max;
-		if ( A[i] > max )
-			max = A[i];
-	}
-	
-	max = A[n-1];
-	for ( int i = n - 2; i >= 0; i-- ){
-		maxright[i] = max;
-		if ( A[i] > max )
-			max = A[i];
-	}
-	
-	for ( int i = 0; i < n - 1; i++ ){
-		int trap = min(maxleft[i], maxright[i]) - A[i];
-		if ( trap > 0 )
-			contain += trap;
-	}
-	return contain;
-}
-
+int firstMissingPositive(int A[], int n) {
+        if ( n <= 0 )
+            return -1;
+        
+        for ( int i = 0; i < n; ){
+			
+            if ( A[i] <= 0 || A[i] > n || A[i] == i+1 )
+                i++;
+            else{
+                int index = A[i]-1;
+				if ( A[i] == A[index]){
+					i++;
+					continue;
+				}
+                int tmp = A[i];
+                A[i] = A[index];
+                A[index] = tmp;
+            }
+			for ( int k = 0; k < n; k++ )
+				cout << A[k] << " ";
+			cout << endl;
+        }
+        
+		for ( int i = 0; i < n; i++ )
+			cout << A[i] << " ";
+		cout << endl;
+		
+        for ( int i = 0; i < n; i++ ){
+            if ( A[i] != (i+1) )
+                return (i+1);
+        }
+        return n+1;
+    }
 int main(){
-	int A[] = {0,1,0,2,1,0,1,3,2,1,2,1};
-	cout <<  trap(A, 12);
-	
+	int A[] = {1,1};
+	cout << firstMissingPositive(A,2) << endl;
 	return 0;
 }
